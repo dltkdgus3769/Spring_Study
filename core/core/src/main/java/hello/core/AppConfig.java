10cfcf181887ff1577326e7ad9034d62c9ae1a2a
LSH <tkdgus3769@naver.com>
2025-03-06 22:25:15 +0900

    250306 공부 내용 추가

--- core/core/src/main/java/hello/core/AppConfig.java
+++ core/core/src/main/java/hello/core/AppConfig.java
@@ -5,20 +5,25 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.*;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepositoy());
     }
-
-    private MemberRepository memberRepositoy() {
+    @Bean
+    public MemberRepository memberRepositoy() {
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepositoy(), discountPolicy());
     }
-
+    @Bean
     public DiscountPolicy discountPolicy(){
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
