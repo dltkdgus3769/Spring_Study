575b77a1429b350c6982bca102258f4308a7216a
LSH <tkdgus3769@naver.com>
2025-03-18 22:01:32 +0900

    내용 추가

---
+++ core/core/src/test/java/hello/core/singleTon/SingleTonTest.java
@@ -0,0 +1,39 @@
+package hello.core.singleTon;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SingleTonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer() {
+        AppConfig appConfig = new AppConfig();
+
+        MemberService memberService = appConfig.memberService();
+        MemberService memberService2 = appConfig.memberService();
+
+        System.out.println("memberService = " + memberService);
+        System.out.println("memberService2 = " + memberService2);
+
+        Assertions.assertThat(memberService).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest(){
+        SingleTonService singleTonService = SingleTonService.getInstance();
+        SingleTonService singleTonService2 = SingleTonService.getInstance();
+        System.out.println("singleTonService = " + singleTonService);
+        System.out.println("singleTonService2 = " + singleTonService2);
+
+
+    }
+
+
+
+}
+
